438a904bdc659535e2f3635ffb8ab541fa68ae66
Bruce Denham <bdenham@adobe.com>
2019-09-01 18:31:39 -0500

    Add lazyloading to Image content type using lazysizes

---
+++ Example/PageBuilderExtensionImage/view/base/web/js/resource/lazysizes/lazySizesConfig.js
@@ -0,0 +1,9 @@
+// This is the recommended way to customize config settings for lazysizes.
+// The default config settings are found here: https://github.com/aFarkas/lazysizes#js-api---options
+// Examples of a few customizations are shown here.
+
+(function () {
+    window.lazySizesConfig = window.lazySizesConfig || {};
+    window.lazySizesConfig.loadMode = 1;
+    window.lazySizesConfig.expand = 0;
+})();
